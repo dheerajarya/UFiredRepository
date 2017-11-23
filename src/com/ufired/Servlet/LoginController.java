d40a067ce1252793cc42b0d8f4a5c5211f6910fd
darya3 <darya3@csc.com>
2017-11-23 11:26:39 +0530

    UFired Login Controller Commit

--- src/com/ufired/Servlet/LoginController.java
+++ src/com/ufired/Servlet/LoginController.java
@@ -28,7 +28,6 @@ extends HttpServlet
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { doPost(request, response); }
 
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException
 	{
